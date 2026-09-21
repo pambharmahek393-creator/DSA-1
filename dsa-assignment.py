0d21d436219045fe42b3278a5f6e38f24604b40a
Atmiya <atmiya@gmail.com>
2026-09-21 08:09:38 +0530

    Add dsa assignment

---
+++ dsa-assignment.py
@@ -0,0 +1,50 @@
+#1. Variables
+
+#1. Create variables to store name, age, and city and display them.
+
+name = "John"
+age = 20
+city = "Rajkot"
+
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+
+
+#2. Swap the values of two variables.
+
+a = 10
+b = 20
+
+a, b = b, a
+
+print("a =", a)
+print("b =", b)
+
+#3. Calculate the area of a rectangle using variables.
+
+length = 10
+width = 5
+
+area = length * width
+
+print("Area of rectangle:", area)
+
+#4. Calculate simple interest using variables.
+
+principal = 10000
+rate = 5
+time = 2
+
+simple_interest = (principal * rate * time) / 100
+
+print("Simple Interest:", simple_interest)
+
+
+#5. Convert Celsius temperature to Fahrenheit.
+
+celsius = 25
+
+fahrenheit = (celsius * 9 / 5) + 32
+
+print("Temperature in Fahrenheit:", fahrenheit)
