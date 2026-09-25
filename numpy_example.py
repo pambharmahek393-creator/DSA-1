dafaf3c811236d9227032ca643270501dc2b756f
pambhar mahek <pambharmahek393@gmail.com>
2026-09-25 08:23:57 +0530

    Add new practical

---
+++ numpy_example.py
@@ -0,0 +1,67 @@
+#1. array()
+
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+
+print("Array:", arr)
+
+
+#2. arange()
+
+import numpy as np
+
+arr = np.arange(1, 11, 2)
+
+print("Array:", arr)
+
+
+#3. linspace()
+
+import numpy as np
+
+arr = np.linspace(0, 10, 5)
+
+print("Array:", arr)
+
+
+#4. logspace()
+
+import numpy as np
+
+arr = np.logspace(1, 3, 5)
+
+print("Array:", arr)
+
+
+#5. zeros()
+
+import numpy as np
+
+arr = np.zeros(5)
+
+print("Zeros:", arr)
+
+#For a 2D array:
+
+import numpy as np
+
+arr = np.zeros((2, 3))
+
+print(arr)
+
+#6. ones()
+
+import numpy as np
+
+arr = np.ones(5)
+
+print("Ones:", arr)
+
+#For a 2D array:
+
+import numpy as np
+
+arr = np.ones((2, 3))
+
+print(arr)
