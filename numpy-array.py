721a4837654f8a328a847415f058c7e5cb50de4d
pambhar mahek <pambharmahek393@gmail.com>
2026-09-25 08:18:02 +0530

    Add new practical

---
+++ numpy-array.py
@@ -0,0 +1,108 @@
+#1. Find Maximum and Minimum
+
+import numpy as np
+
+arr = np.array([10, 25, 5, 40, 15])
+
+print("Array:", arr)
+print("Maximum:", arr.max())
+print("Minimum:", arr.min())
+
+
+#2. Sum of Array
+
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+
+print("Array:", arr)
+print("Sum:", arr.sum())
+
+
+#3. Calculate Mean and Median
+
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+
+print("Mean:", np.mean(arr))
+print("Median:", np.median(arr))
+
+#4. Multiply Every Element
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+result = arr * 2
+
+print("Original:", arr)
+print("After multiplication:", result)
+
+
+#5. Add Two Arrays
+
+import numpy as np
+
+a = np.array([10, 20, 30])
+b = np.array([5, 10, 15])
+
+result = a + b
+
+print("A:", a)
+print("B:", b)
+print("Addition:", result)
+
+#6. Create 2D Array
+
+import numpy as np
+
+arr = np.array([
+    [1, 2, 3],
+    [4, 5, 6]
+])
+
+print(arr)
+print("Shape:", arr.shape)
+
+
+#7. Create Array Using arange()
+
+import numpy as np
+
+arr = np.arange(1, 11)
+
+print(arr)
+
+
+#8. Square of Each Number
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print("Square:", arr ** 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
